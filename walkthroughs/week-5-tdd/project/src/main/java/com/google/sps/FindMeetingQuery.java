abf8512d5e28a8f61e12549ed3b7ac756836782b
Angela Liu <aliu917@berkeley.edu>
2020-06-05 20:55:33 +0000

    Finished implementing query method for FinMeetingQuery.

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,42 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    ArrayList<TimeRange> eventTimes = new ArrayList<>();
+    for (Event event : events) {
+      if (Collections.disjoint(event.getAttendees(), request.getAttendees())) {
+        continue;
+      }
+      eventTimes.add(event.getWhen());
+    }
+    Collections.sort(eventTimes, TimeRange.ORDER_BY_START);
+    ArrayList<TimeRange> availableRanges = findAvailableRanges(eventTimes, TimeRange.START_OF_DAY);
+    ArrayList<TimeRange> availableRangesClone = (ArrayList<TimeRange>) availableRanges.clone();
+    for (TimeRange range : availableRangesClone) {
+      if (range.duration() < request.getDuration()) {
+        availableRanges.remove(range);
+      }
+    }
+    return availableRanges;
+  }
+
+  private ArrayList<TimeRange> findAvailableRanges(ArrayList<TimeRange> eventTimes, int prevEnd) {
+    ArrayList<TimeRange> availableTimes = new ArrayList<>();
+    for (int i = 0; i < eventTimes.size(); i++) {
+      TimeRange time = eventTimes.get(i);
+      if (time.start() > prevEnd) {
+        availableTimes.add(TimeRange.fromStartEnd(prevEnd, time.start(), false));
+      }
+      prevEnd = Math.max(prevEnd, time.end());
+    }
+    availableTimes.add(TimeRange.fromStartEnd(prevEnd, TimeRange.END_OF_DAY, true));
+    return availableTimes;
   }
 }
